415543e97f08110b37283cfde4c0eb9b84bce7eb
Ruth Olatunji <70859087+rutholatunji@users.noreply.github.com>
2020-11-25 11:30:52 +0000

    Contact page update Updated, contact page, added url, email fields so the user can submit their podcast

--- js/contact.js
+++ js/contact.js
@@ -1,14 +1,17 @@
 let alreadySubmitted = false;
 
-function appendModal(nameValue, topicValue, messageValue) {
+function appendModal(nameValue, emailValue, urlValue, messageValue) {
   if (alreadySubmitted === true) {
     //remove elements
 
     const usedNameField = document.getElementById("nameField");
     usedNameField.remove();
 
-    const usedTopicField = document.getElementById("topicField");
-    usedTopicField.remove();
+    const usedEmailField = document.getElementById("emailField");
+    usedEmailField.remove();
+
+    const usedUrlField = document.getElementById("urlField");
+    usedUrlField.remove();
 
     const usedMessageField = document.getElementById("messageField");
     usedMessageField.remove();
@@ -21,10 +24,15 @@ function appendModal(nameValue, topicValue, messageValue) {
   document.getElementById("nameDiv").appendChild(nameField);
 
   // Add topic text to modal
-  const topicField = document.createElement("p");
-  topicField.setAttribute("id", "topicField");
-  topicField.textContent = topicValue;
-  document.getElementById("topicDiv").appendChild(topicField);
+  const emailField = document.createElement("p");
+  emailField.setAttribute("id", "emailField");
+  emailField.textContent = emailValue;
+  document.getElementById("emailDiv").appendChild(emailField);
+
+  const urlField = document.createElement("p");
+  urlField.setAttribute("id", "urlField");
+  urlField.textContent = urlValue;
+  document.getElementById("urlDiv").appendChild(urlField);
 
   // Add message text to modal
   const messageField = document.createElement("p");
@@ -37,8 +45,9 @@ function appendModal(nameValue, topicValue, messageValue) {
 
 appendModal(
   "Code First Girls",
-  "New Website",
-  "We want a new website, please help!"
+  "codefirstgirls@mail.com",
+  "https://codefirstgirls.org.uk/",
+  "Add our podcast to your website please!"
 );
 
 const form = document.getElementById("contactForm");
@@ -47,11 +56,11 @@ form.addEventListener("submit", function (event) {
   event.preventDefault();
 
   const nameField = document.getElementsByName("f_name")[0].value;
-  const topicField = document.getElementsByName("f_topic")[0].value;
+  const emailField = document.getElementsByName("f_email")[0].value;
+  const urlField = document.getElementsByName("f_url")[0].value;
   const messageField = document.getElementsByName("f_message")[0].value;
 
-  appendModal(nameField, topicField, messageField);
+  appendModal(nameField, emailField, urlField, messageField);
 });
 
 // Add event listener
-
