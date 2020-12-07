d0ce822d44cc4653e47ee7be4978e8f6fe06bb80
Ruth Olatunji <70859087+rutholatunji@users.noreply.github.com>
2020-12-07 15:05:24 +0000

    Updated form and added javascript

--- js/contact.js
+++ js/contact.js
@@ -3,8 +3,10 @@ let alreadySubmitted = false;
 function appendModal(
   nameValue,
   emailValue,
-  urlValue,
+  podcastValue,
   categoryValue,
+  optionValue,
+  episodeValue,
   messageValue
 ) {
   if (alreadySubmitted === true) {
@@ -16,11 +18,17 @@ function appendModal(
     const usedEmailField = document.getElementById("emailField");
     usedEmailField.remove();
 
-    const usedUrlField = document.getElementById("urlField");
-    usedUrlField.remove();
+    const usedPodcastField = document.getElementById("podcastField");
+    usedPodcastField.remove();
 
     const usedCategoryField = document.getElementById("categoryField");
     usedCategoryField.remove();
+
+    const usedOptionField = document.getElementById("optionField");
+    usedOptionField.remove();
+
+    const usedEpisodeField = document.getElementById("episodeField");
+    usedEpisodeField.remove();
 
     const usedMessageField = document.getElementById("messageField");
     usedMessageField.remove();
@@ -38,15 +46,25 @@ function appendModal(
   emailField.textContent = emailValue;
   document.getElementById("emailDiv").appendChild(emailField);
 
-  const urlField = document.createElement("p");
-  urlField.setAttribute("id", "urlField");
-  urlField.textContent = urlValue;
-  document.getElementById("urlDiv").appendChild(urlField);
+  const podcastField = document.createElement("p");
+  podcastField.setAttribute("id", "podcastField");
+  podcastField.textContent = podcastValue;
+  document.getElementById("podcastDiv").appendChild(podcastField);
 
   const categoryField = document.createElement("p");
   categoryField.setAttribute("id", "categoryField");
   categoryField.textContent = categoryValue;
   document.getElementById("categoryDiv").appendChild(categoryField);
+
+  const optionField = document.createElement("p");
+  optionField.setAttribute("id", "optionField");
+  optionField.textContent = optionValue;
+  document.getElementById("optionDiv").appendChild(optionField);
+
+  const episodeField = document.createElement("p");
+  episodeField.setAttribute("id", "episodeField");
+  episodeField.textContent = episodeValue;
+  document.getElementById("episodeDiv").appendChild(episodeField);
 
   // Add message text to modal
   const messageField = document.createElement("p");
@@ -72,11 +90,21 @@ form.addEventListener("submit", function (event) {
 
   const nameField = document.getElementsByName("f_name")[0].value;
   const emailField = document.getElementsByName("f_email")[0].value;
-  const urlField = document.getElementsByName("f_url")[0].value;
+  const podcastField = document.getElementsByName("f_url")[0].value;
   const categoryField = document.getElementsByName("f_category")[0].value;
+  const optionField = document.getElementsByName("f_option")[0].value;
+  const episodeField = document.getElementsByName("ff_url")[0].value;
   const messageField = document.getElementsByName("f_message")[0].value;
 
-  appendModal(nameField, emailField, urlField, categoryField, messageField);
+  appendModal(
+    nameField,
+    emailField,
+    podcastField,
+    categoryField,
+    optionField,
+    episodeField,
+    messageField
+  );
 
   $("#exampleModal").modal();
 });
