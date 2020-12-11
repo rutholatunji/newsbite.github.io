a17840d8c6e2abf3cfb0a6f9f38331836adfaf44
Ruth Olatunji <70859087+rutholatunji@users.noreply.github.com>
2020-12-11 13:23:38 +0000

    php practice

--- index.php
+++ index.php
@@ -2,3 +2,46 @@
 
 <?php
 echo "Hello, World!";
+
+// escape sequences
+echo "She said \"hi\" to the dog."; 
+
+
+// Create to do list
+echo "1. Eat breakfast";
+echo "\n2. Go to work";
+echo "\n3. Eat a \"healthy\" snack";
+echo "Code"."cademy";
+echo "\nMy name is:"." Ruth";
+echo "\n"."tur"."duck"."en";
+
+// Excersize 1
+$favorite_snack = "cupcake";  
+
+$biography = "\n I am 25 years old and I like eggs";
+
+$favorite_food = "\n"."tur"."duck"."en";
+  
+echo $biography;
+
+// Excersize 2
+$name = "Ruth";
+$language = "PHP";
+  
+echo "My name is ".$name;
+
+echo "\n I'm learning ".$language;
+
+// Exercise 3
+// Fill in the blanks in the code below:
+  $noun = "life";
+  $adjective = "sweet";
+  $verb = "likes";
+
+  echo "The world's most beloved ${noun} was very ${adjective} and loved to ${verb} every single day.";
+
+
+//Fix the code below and uncomment it:
+
+echo "\nI have always been obsessed with ${noun}s. I'm ${adjective}ish. I'm always ${verb}ing.";
+
