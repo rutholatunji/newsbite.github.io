054ea202a7bc95ee3165fcbe3567515f93364a23
Ruth Olatunji <ruth.o2009@live.co.uk>
2021-02-23 13:42:07 +0000

    updated registration links

---
+++ Registration/Includes/header.php
@@ -0,0 +1,189 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+		<meta http-equiv="X-UA-Compatible" content="ie=edge" />
+		<!-- BOOTSTRAP CSS -->
+		<link
+			rel="stylesheet"
+			href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+			integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T"
+			crossorigin="anonymous"
+		/>
+
+		<!-- MAIN CSS --> 
+
+		<link rel="stylesheet" href="./css/main.css" />
+		<link
+			rel="stylesheet"
+			href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"
+		/>
+		<title>Newsbite - Homepage</title>
+	</head>
+
+	<body>
+<header>
+<!-- 1ST NAVIGATION -->
+			<nav class="navbar navbar-expand-lg navbar navbar-dark " 
+			style="background-color: black;">
+			
+			<button 
+			class="navbar-toggler" 
+			type="button" 
+			data-toggle="collapse" 
+			data-target="#navbarTogglerDemo03" 
+			aria-controls="navbarTogglerDemo03" 
+			aria-expanded="false" 
+			aria-label="Toggle navigation">
+					<span class="navbar-toggler-icon"></span>
+				</button>
+			
+			
+				<div class="collapse navbar-collapse" 
+				id="navbarTogglerDemo03">
+					<ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+						<li class="nav-item active">
+							<a class="nav-link" 
+							href="./index.html">Home <span class="sr-only">(current)</span></a>
+						</li>
+						<li class="nav-item">
+							<a class="nav-link" 
+							href="./healthandfitness.html">Health & Fitness</a>
+						</li>
+						<li class="nav-item">
+							<a class="nav-link disabled" 
+							href="./contact.html">Arts</a>
+						</li>
+						<li class="nav-item">
+							<a class="nav-link disabled" 
+							href="./contact.html">Education</a>
+						</li>
+						<li class="nav-item">
+							<a class="nav-link disabled" 
+							href="./contact.html">Music</a>
+						</li>
+
+						<li class="nav-item">
+							<a class="nav-link disabled" 
+							href="./contact.html">Science</a>
+						</li>
+						<li class="nav-item">
+							<a class="nav-link disabled" 
+							href="./contact.html">Technology</a>
+						</li>
+						<li class="nav-item">
+								<a class="nav-link disabled" 
+								href="./contact.html">True Crime</a>
+							</li>
+						
+								<li class="nav-item">
+										<a class="nav-link disabled" 
+										href="./contact.html">Entertainment</a>
+									</li>
+					
+						 <li class="nav-item">
+									<a class="nav-link disabled" 
+									href="./contact.html">News</a>
+								</li> 
+
+								<li class="nav-item">
+										<a class="nav-link" 
+										href="./login.html">Login</a>
+									</li> 
+
+								<li class="nav-item">
+									<a class="nav-link" 
+									href="./register.html">Register</a>
+								</li> 
+
+							<li class="nav-item">
+								<a class="nav-link" 
+								href="./contact.html">Contact</a>
+							</li>
+					
+					
+					</ul>
+					
+					<div class="a-inline my-2 my-lg-0">
+							<a href="https://www.instagram.com/"><i 
+								class="fa fa-instagram" 
+								aria-hidden="true"></i></a>
+							<a href="https://www.facebook.com/"><i 
+								class="fa fa-facebook-square" 
+								aria-hidden="true"></i></a>
+							<a href="https://www.linkedin.com/in/ruth-olatunji-249588149/"><i 
+								class="fa fa-linkedin-square" 
+								aria-hidden="true"></i></a>
+				</div>
+				</div>
+			</nav>
+	<!-- 1ST NAVIGATION ENG -->
+
+<!-- LOGO  -->
+
+	<div class="container">
+		<div class="row">
+		<div class="col-sm-12 col-md-12 col-lg-12">
+	
+<img
+class="logo" 
+src="./images/LOGO.png" />
+		</div>
+	</div>
+	</div>
+	
+	<!-- LOGO end -->
+	
+	<!-- 2ND NAV BAR -->
+	<div class="container">
+		<div class="row">
+		<div class="col-sm-12 col-md-12 col-lg-12">
+
+			<nav class="navbar navbar-expand-lg navbar-light"
+			style="background-color: white">
+			<button class="navbar-toggler" 
+			type="button" 
+			data-toggle="collapse" 
+			data-target="#navbarTogglerDemo03" 
+			aria-controls="navbarTogglerDemo03" 
+			aria-expanded="false" 
+			aria-label="Toggle navigation">
+					<span class="navbar-toggler-icon"></span>
+				</button>
+			
+			
+				<div class="collapse navbar-collapse" 
+				id="navbarTogglerDemo03">
+					<ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+						<li class="nav-item active">
+							<a class="nav-link active" 
+							href="./newshomepage.html">Home <span 
+							class="sr-only">(current)</span></a>
+						</li>
+						<li class="nav-item">
+							
+							<script language="JavaScript">var fhs = document.createElement('script');var fhs_id = "5652087";
+								var ref = (''+document.referrer+'');var pn =  window.location;var w_h = window.screen.width + " x " + window.screen.height;
+								fhs.src = "//freehostedscripts.net/ocounter.php?site="+fhs_id+"&e1=Online User&e2=Online Users&r="+ref+"&wh="+w_h+"&a=1&pn="+pn+"";
+								document.head.appendChild(fhs);document.write("<span id='o_"+fhs_id+"'></span>");
+								</script>
+				
+						</li>
+					
+							
+					</ul>
+					<form class="form-inline my-2 my-lg-0">
+						<input class="form-control mr-sm-2"
+						 type="search" 
+						 placeholder="Search" 
+						 aria-label="Search"
+						 id="anythingSearch">
+						<button class="btn btn-outline-success my-2 my-sm-0" 
+						type="submit">Search</button>
+					</form>
+				</div>
+			</nav>
+				<!-- 2ND NAV BAR END -->
+				
+		</header>
